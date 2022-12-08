b0c7b76aa6921c7bd94b79c3abd75a5db5d50f26
bethdecarlo <bethdecarlo@gmail.com>
2022-12-08 13:47:57 -0500

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 //Header, Footer, About, Resume, Contact, Portfolio
 import React, { Component } from 'react';
 import $ from 'jquery';
-import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import About from './Components/About';

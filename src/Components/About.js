e63617004e9cc2bce522be6644f5457c758407c1
Beth DeCarlo <110702442+bethdecarlo@users.noreply.github.com>
2022-12-10 00:50:47 -0500

    cleaned up code

--- src/Components/About.js
+++ src/Components/About.js
@@ -38,7 +38,7 @@ class About extends Component {
                </div>
                <div className="columns download">
                   <p>
-                     <a href={resumeDownload} className="button" target="_blank"><i class="fa fa-pdf"></i>Download Resume</a>
+                     <a href={resumeDownload} className="button" target="_blank"><i class="fa fa-download"></i>Download Resume</a>
                   </p>
                </div>
             </div>
